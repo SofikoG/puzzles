7eb39773e8e614e3c494007ef4a9ceb39d8b7aa6
unknown <Sophia>
2023-02-05 12:43:33 +0300

    добавление элементика

--- app.js
+++ app.js
@@ -2,48 +2,44 @@ const inp = document.querySelector('.inp')
 const btn = document.querySelector('.btn')
 const question = document.querySelector('.question')
 let count = 1
+let rightAnswers = 0
+const great = document.querySelector('.greatAnswers')
+const questions = [
+    `Вот иголки и булавки Выползают из-под лавки, На меня они глядят, Молока они хотят.`,
+    `Что принадлежит вам, но другие используют это чаще?`,
+    `Каких камней нет ни в одном море?`,
+    `Что можно видеть с закрытыми глазами?`,
+]
+const answers = ['ёж', 'имя', 'сухих', 'сон']
+
+function checkAnswer() {
+    let answer = inp.value
+    if (answer == answers[count - 1]) {
+        alert('Аплодисменты!')
+        rightAnswers++
+        great.innerHTML = `${rightAnswers}/${questions.length}`
+    } else {
+        alert('Не-а!')
+    }
+
+    inp.value = ''
+    if (count == 4) {
+        question.innerHTML = 'Конец'
+    } else {
+        question.innerHTML = questions[count]
+    }
+
+    count++
+}
 
 btn.addEventListener('click', () => {
     if (count == 1) {
-        let answer = inp.value
-        if (answer == "ёж") {
-            alert('Аплодисменты!')
-        } else {
-            alert('Не-а!')
-        }
-
-        inp.value = ''
-        question.innerHTML = `Что принадлежит вам, но другие используют это чаще?`
-        count++
+        checkAnswer()
     } else if (count == 2) {
-        let answer = inp.value
-        if (answer == "имя") {
-            alert('Молодец!')
-        } else {
-            alert('Не угадал!')
-        }
-        inp.value = ''
-        question.innerHTML = `Каких камней нет ни в одном море?`
-        count++
+        checkAnswer()
     } else if (count == 3) {
-        let answer = inp.value
-        if (answer == "сухих") {
-            alert('Правильно!')
-        } else {
-            alert('Не правильно!')
-        }
-        inp.value = ''
-        question.innerHTML = `Что можно видеть с закрытыми глазами?`
-        count++
+        checkAnswer()
     } else if (count == 4) {
-        let answer = inp.value
-        if (answer == "сон") {
-            alert('Вау,правильно!')
-        } else {
-            alert('Нет!')
-        }
-        inp.value = ''
-        question.innerHTML = `Конец!`
-        count++
+        checkAnswer()
     }
 })
